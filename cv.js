ab29101f5107d4e7c82b945eb7dd8e63723fa678
maldiprog <126576711+maldiprog@users.noreply.github.com>
2023-02-28 21:54:06 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,15 @@
+console.log("hola mundo");
+
+function verificarmenu() {
+    if (document.getElementById('menu').value=="Fortalezas")
+        
+    document.getElementById('fortaleza').innerHTML="qqqqqqqqqqqqqq";}
+    
+    { if (document.getElementById('menu').value=="Debilidades")
+    
+    document.getElementById('debilidad').innerHTML="xxxxxxx";
+    }
+    
+    
+
+
